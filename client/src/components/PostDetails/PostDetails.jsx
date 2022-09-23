7e3f3e39d272532b00efaa9ae9c4f6de6d8daf56
kxnyshk <ikanishk1172@gmail.com>
2022-09-23 23:07:12 +0530

    6:34:45 (updated posts' body / fixed media maxHeight)

--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
 import { useParams, useHistory } from 'react-router-dom';
 import useStyles from './styles.js';
-import {getPost} from '../../actions/posts.js';
+import {getPost, getPostsBySearch} from '../../actions/posts.js';
 
 const PostDetails = () => {
     const { post, posts, isLoading } = useSelector((state) => state.posts);
@@ -17,15 +17,13 @@ const PostDetails = () => {
         dispatch(getPost(id));
     }, [id]);
 
-    // useEffect(() => {
-    //     if (post) {
-    //     dispatch(getPostsBySearch({ search: 'none', tags: post?.tags.join(',') }));
-    //     }
-    // }, [post]);
+    useEffect(() => {
+        if (post) {
+        dispatch(getPostsBySearch({ search: 'none', tags: post?.tags.join(',') }));
+        }
+    }, [post]);
 
     if (!post) return null;
-
-    // const openPost = (_id) => history.push(`/posts/${_id}`);
 
     if (isLoading) {
         return (
@@ -34,6 +32,9 @@ const PostDetails = () => {
             </Paper>
         );
     };
+    
+    const recommendedPosts = posts.filter(({ _id }) => _id !== post._id);
+    const openPost = (_id) => history.push(`/posts/${_id}`);
 
     return (
         <Paper style={{padding: '20px', borderRadius: '15px'}} elevation={6}>
@@ -54,8 +55,28 @@ const PostDetails = () => {
                     <img className={classes.media} src={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} alt={post.title} />
                 </div>
             </div>
+
+            {/* recommended posts below */}
+            {!!recommendedPosts.length && (
+                <div className={classes.section}>
+                    <Typography gutterBottom variant="h5">You might also like:</Typography>
+                    <Divider />
+                    <div className={classes.recommendedPosts}>
+                        {recommendedPosts.map(({ title, name, message, likes, selectedFile, _id }) => (
+                            <div style={{ margin: '20px', cursor: 'pointer' }} onClick={() => openPost(_id)} key={_id}>
+                                <Typography gutterBottom variant="h6">{title}</Typography>
+                                <Typography gutterBottom variant="subtitle2">{name}</Typography>
+                                <Typography gutterBottom variant="subtitle2">{message}</Typography>
+                                <Typography gutterBottom variant="subtitle1">Likes: {likes?.length}</Typography>
+                                <img src={selectedFile} width="200px" />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )} {/* recommended posts above */}
+
         </Paper>
     );
-}
+};
 
 export default PostDetails;
